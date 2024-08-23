d957bde8eb07e9c5364be3fb3960f94cf2ca3edc
usarvesh-at-148000812951 <usarvesh1995@gmail.com>
2024-08-23 12:48:03 +1000

    credentialsId

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,5 +1,5 @@
-def call(String branch,String repoURL){
+def call(String branch,String repoURL,String credentialsId){
 
-checkout scmGit(branches: [[name: branch]], extensions: [], userRemoteConfigs: [[url: repoURL]])
+checkout scmGit(branches: [[name: branch]], extensions: [], userRemoteConfigs: [[url: repoURL,credentialsId:credentialsId]])
 
 }
