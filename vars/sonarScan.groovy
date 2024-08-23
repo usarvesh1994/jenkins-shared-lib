924faadff546de51c68ba7b094c5b8f65d7a8813
usarvesh-at-148000812951 <usarvesh1995@gmail.com>
2024-08-23 13:36:14 +1000

    SCANNER_HOME

--- vars/sonarScan.groovy
+++ vars/sonarScan.groovy
@@ -1,5 +1,5 @@
-call def(){
+call def(String SCANNER_HOME){
     withSonarQubeEnv('sonarqube') {
-                sh "${env.SCANNER_HOME}/bin/sonar-scanner -Dsonar.projectName=Youtube -Dsonar.projectKey=Youtube -Dsonar.sources=src"
+                sh "${SCANNER_HOME}/bin/sonar-scanner -Dsonar.projectName=Youtube -Dsonar.projectKey=Youtube -Dsonar.sources=src"
             }
 }
